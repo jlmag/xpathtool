bb10b95c6784e541bfd7ad042c15a5dae86a995c
Jean-Luc MAGNAN <jlmagnan@live.fr>
2012-10-28 21:33:46 +0100

    Create projet xpathtool

---
+++ source/class/xpathtool/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("xpathtool.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ source/class/xpathtool/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("xpathtool.theme.Theme",
+{
+  meta :
+  {
+    color : xpathtool.theme.Color,
+    decoration : xpathtool.theme.Decoration,
+    font : xpathtool.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : xpathtool.theme.Appearance
+  }
+});

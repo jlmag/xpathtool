641e7be5c46d547f476e9ff8a9ab3d7b4e27ce62
Jean-Luc MAGNAN <jlmagnan@live.fr>
2012-10-29 15:51:41 +0100

    Update Application.js

--- source/class/xpathtool/Application.js
+++ source/class/xpathtool/Application.js
@@ -39,7 +39,6 @@ qx.Class.define("xpathtool.Application",
      */
     main : function()
     {
-      // Call super class
       this.base(arguments);
 
       // Enable logging in debug variant
@@ -50,26 +49,7 @@ qx.Class.define("xpathtool.Application",
         // support additional cross-browser console. Press F7 to toggle visibility
         qx.log.appender.Console;
       }
-
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
-
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "xpathtool/test.png");
-
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+      
     }
   }
 });
